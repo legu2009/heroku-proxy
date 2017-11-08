a50c54eb27a1af142e1a46a217d0f9e32ad8fffe
legu2009 <89415119@qq.com>
2017-11-08 17:26:21 +0800

    根据管道修改

--- anyproxy/proxy.js
+++ anyproxy/proxy.js
@@ -2,46 +2,36 @@
 
 var http = require('http');
 var AnyProxy = require('anyproxy');
-var _protocol = 'http';
-var _host = 'wgu.eu-4.evennode.com';
 
 
 var _protocol = 'https';
 var _host = 'wgu.herokuapp.com';
 var _host = 'wgu-legu2009.c9users.io';
 
-
 var _port = _protocol === 'https' ? '443' : '80';
-//_protocol = 'http';
-//_host = 'localhost';
-//_port = '8090';
+
+/*
+_protocol = 'http';
+_host = 'localhost';
+_port = '8090';
+*/
 
 var rule = {
     summary: 'modify response',
-    * beforeSendRequest(requestDetail, responseDetail) {
-        if (requestDetail.requestOptions.hostname === _host) return;
-        delete requestDetail.requestOptions.headers['Accept-Encoding'];
+    dealProxyOptions(requestDetail) {
+        var requestOptions = requestDetail.requestOptions;
+        var origin = {
+            hostname: requestOptions.hostname,
+            port: requestOptions.port,
+            protocol: requestDetail.protocol
+        }
+        requestOptions.hostname = requestOptions.headers.Host = _host;
+        requestOptions.port = _port;
+        requestOptions.headers['proxy_wgu_Origin'] = JSON.stringify(origin);
         return {
             protocol: _protocol,
-            requestOptions: {
-                hostname: _host,
-                port: _port,
-                path: '/',
-                method: 'POST',
-                headers: {
-                    'Host': _host,
-                    'Accept': 'text/plain, */*; q=0.01',
-                    'Origin': _protocol + '://' + _host,
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
-                }
-            },
-            requestData: JSON.stringify({
-                requestOptions: requestDetail.requestOptions,
-                requestData: requestDetail.requestData.toString(),
-                protocol: requestDetail.protocol
-            })
-        };
+            requestOptions
+        }
     },
 };
 
--- app.js
+++ app.js
@@ -1,31 +1,74 @@
 const http = require('http'),
-    https = require('https');
+    https = require('https'),
+    url = require('url');
 
-http.createServer(function (req, res) {
-    const rawResChunks = [];
-    req.on('data', (chunk) => {
-        rawResChunks.push(chunk);
-    });
-    req.on('end', () => {
-        var str = rawResChunks.join('');
-        if (!str) {
-            res.writeHead('200', { 'Content-Type': 'text/plain' });
-            res.end('hello');
-            return;
+
+var getHeaderFromRawHeaders = function (rawHeaders) {
+    const headerObj = {};
+    const _handleSetCookieHeader = function (key, value) {
+        if (headerObj[key].constructor === Array) {
+            headerObj[key].push(value);
+        } else {
+            headerObj[key] = [headerObj[key], value];
         }
-        var config = JSON.parse(str);
-        var {
-            protocol,
-            requestOptions,
-            requestData
-        } = config;
-        const proxyReq = (/https/i.test(protocol) ? https : http).request(requestOptions, proxyRes => {
+    };
+    if (!!rawHeaders) {
+        for (let i = 0; i < rawHeaders.length; i += 2) {
+            const key = rawHeaders[i];
+            let value = rawHeaders[i + 1];
+            if (typeof value === 'string') {
+                value = value.replace(/\0+$/g, ''); // 去除 \u0000的null字符串
+            }
+            if (!headerObj[key]) {
+                headerObj[key] = value;
+            } else {
+                if (key.toLowerCase() === 'set-cookie') {
+                    _handleSetCookieHeader(key, value);
+                } else {
+                    headerObj[key] = headerObj[key] + ',' + value;
+                }
+            }
+        }
+    }
+    return headerObj;
+};
+http.createServer(function (req, res) {
+
+    const host = req.headers.host;
+    let protocol = (!!req.connection.encrypted && !(/^http:/).test(req.url)) ? 'https' : 'http';
+    const fullUrl = protocol === 'http' ? req.url : (protocol + '://' + host + req.url);
+
+    const urlPattern = url.parse(fullUrl);
+    const path = urlPattern.path;
+
+    req.headers = getHeaderFromRawHeaders(req.rawHeaders);
+
+    const options = {
+        hostname: urlPattern.hostname || req.headers.host,
+        port: urlPattern.port || req.port || (/https/.test(protocol) ? 443 : 80),
+        path,
+        method: req.method,
+        headers: req.headers
+    };
+    var origin = req.headers['proxy_wgu_origin'];
+    if (!origin) {
+        res.writeHead('200', {
+            'Content-Type': 'text/plain'
+        });
+        res.end('hello');
+    } else {
+        delete req.headers['proxy_wgu_origin'];
+        origin = JSON.parse(origin);
+        protocol = origin.protocol;
+        options.port = origin.port;
+        options.hostname = options.headers.Host = origin.hostname;
+        const proxyReq = (/https/i.test(protocol) ? https : http).request(options, proxyRes => {
             res.writeHead(proxyRes.statusCode, proxyRes.headers);
             proxyRes.pipe(res);
         });
+        req.pipe(proxyReq);
         proxyReq.on('error', (error) => {
             res.end();
         });
-        proxyReq.end(requestData);
-    });
-}).listen(process.env.PORT);
+    }
+}).listen(process.env.PORT || 8090);
